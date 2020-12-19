db2d10ae94a46dc92543ea6ba371343ab62bc7de
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2020-12-19 20:42:37 +0900

    Update BaekJoon_1110.py

--- Basic/BaekJoon_1110.py
+++ Basic/BaekJoon_1110.py
@@ -1,3 +1,4 @@
+#ver1.
 X = int(input())
 
 a = X // 10
@@ -10,5 +11,22 @@ while result != X:
   a = result // 10
   b = result % 10
   result = (b*10) + (a + b)%10
+
+print(cnt)
+
+
+#ver2. 중복된 코드 제거
+X = int(input())
+
+a = X // 10
+b = X % 10
+
+cnt = 0
+
+while result != X:
+  result = (b*10) + (a + b)%10
+  cnt += 1
+  a = result // 10
+  b = result % 10
 
 print(cnt)
