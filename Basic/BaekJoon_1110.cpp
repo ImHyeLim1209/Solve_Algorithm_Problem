06260937395db51eac170eba137be79b10a299e0
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2020-12-19 20:45:06 +0900

    Update BaekJoon_1110.cpp

--- Basic/BaekJoon_1110.cpp
+++ Basic/BaekJoon_1110.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
-#include <string>
 
 int main() {
   int X;
   int a,b;
   int result;
-  int cnt = 1;
+  int cnt = 0;
 
   scanf("%d", &X);
 
   a = X / 10;
   b = X % 10;
-  result = (b*10) + ((a+b)%10);
 
   while(result != X){
     cnt++;
+    result = (b*10) + ((a+b)%10);
     a = result / 10;
     b = result % 10;
-    result = (b*10) + ((a+b)%10);
   }
 
   printf("%d", cnt);
