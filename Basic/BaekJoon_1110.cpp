578475e7c321dcd598884b52ba1e6754fa9e1373
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2020-12-19 20:54:50 +0900

    Update BaekJoon_1110.cpp

--- Basic/BaekJoon_1110.cpp
+++ Basic/BaekJoon_1110.cpp
@@ -1,3 +1,4 @@
+//ver1. while(result != X) 조건으로 풀기
 #include <iostream>
 
 int main() {
@@ -18,5 +19,51 @@ int main() {
     b = result % 10;
   }
 
+  printf("%d", cnt);
+}
+
+
+//ver2. while(true) 조건으로 풀기
+int main() {
+  int X;
+  int a,b;
+  int result;
+  int cnt = 0;
+
+  scanf("%d", &X);
+  result = X;
+
+  while(true){
+    cnt++;
+    a = result / 10;
+    b = result % 10;
+    result = (b*10) + ((a+b)%10);
+    
+    if(result == X)
+      break;
+  }
+
+  printf("%d", cnt);
+}
+
+
+//ver3. do-While로 풀기(do-while은 조건 상관없이 최소 1회 반복문을 실행한 뒤 조건을 따진다)
+int main() {
+  int X;
+  int a,b;
+  int result;
+  int cnt = 0;
+
+  scanf("%d", &X);
+
+  result = X;
+
+  do{
+    cnt++;
+    a = result / 10;
+    b = result % 10;
+    result = (b*10) + ((a+b)%10);
+    }while(result != X);
+    
   printf("%d", cnt);
 }
