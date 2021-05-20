312099590ad8b81d64522ba1b6375c1fc2ff1226
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2021-05-20 18:14:29 +0900

    Update Programmers_30_12905.js

--- graph/Programmers_30_12905.js
+++ graph/Programmers_30_12905.js
@@ -1,5 +1,6 @@
 //https://programmers.co.kr/learn/courses/30/lessons/12905
 //제출답안1. 시간초과
+//모든 좌표를 방문하면서 해당좌표를 왼쪽 상단 시작점으로 하는 정사각형을 구해나간다. 1 > 4 > 9 > 16....
 function solution(board) {
   const isValidPos = (row, col) => {
     return board[row] !== undefined && board[row][col] !== undefined;
@@ -30,4 +31,29 @@ function solution(board) {
   return max * max;
 }
 
+
 //제출답안2
+//정사각형으로 4칸 중 최소인 값 + 1이 현재 범위에서의 최대 정사각형이다.
+function solution(board) {
+  const copyMatrix = (matrix) => {
+    const result = [];
+    matrix.forEach((line) => {
+      const row = [];
+      for (let i = 0; i < line.length; i++) row.push(line[i]);
+      result.push(row);
+    })
+    return result;
+  }
+
+  const matrix = copyMatrix(board);
+  let max = matrix[0][0];
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== 0) {
+        matrix[i][j] = Math.min(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i][j - 1]) + 1;
+        max = Math.max(max, matrix[i][j]);
+      }
+    }
+  }
+  return max ** 2;
+}
