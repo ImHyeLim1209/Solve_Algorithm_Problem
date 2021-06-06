1915c80f255798bbe33f61b1976d758b5e4050e5
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2021-06-06 23:52:09 +0900

    Update Programmers_30_42579.js

--- Level3/Hash/Programmers_30_42579.js
+++ Level3/Hash/Programmers_30_42579.js
@@ -1,16 +1,13 @@
 //https://programmers.co.kr/learn/courses/30/lessons/42579
-//제출답안: 장르별 sort가 아직 구현이 안되었음
+//제출답안: 
+//1. 장르 별로 노래의 총 재생횟수 구하기
+//2. 장르 내에서 재생 순으로 수록
+//3. 재생 횟수가 같다면 번호순으로 수록
 function solution(genres, plays) {
-  //1. 장르 별로 노래의 총 재생횟수 구하기
-  //2. 장르 내에서 재생 순으로 수록
-  //3. 재생 횟수가 같다면 번호순으로 수록
   const hash = {};
   const N = genres.length;
 
-  const swap = (a, b) => {
-    [a, b] = [b, a];
-  }
-
+  //항상 2개씩만 남길 것
   const getHighestTwoSongs = (songIdxs, newOne, plays) => {
     if (songIdxs.length < 2) {
       const songIdx = songIdxs[0];
@@ -28,6 +25,10 @@ function solution(genres, plays) {
       hash[genre][1] = getHighestTwoSongs(hash[genre][1], i, plays);
     }
   }
-
-  return Object.values(hash).reduce((acc, cur) => { return [...acc, ...cur[1]] }, []);
+  //마지막에 장르별 노래 갯수 순으로 출력해야하므로 별개의 배열 priorities를 만듦([['pop', 3100], ['classic', 1450]])
+  const priorities = Object.entries(hash).map(([key, value]) => [key, value[0]]).sort((a, b) => b[1] - a[1]);
+  return priorities.reduce((acc, cur) => {
+    const [genre, _] = cur;
+    return [...acc, ...hash[genre][1]];
+  }, []);
 }
