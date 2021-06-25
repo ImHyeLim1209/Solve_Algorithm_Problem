4737ee2df285d32ed6890b8e6ddd9003b1af0d6e
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2021-06-25 21:40:37 +0900

    Update Codility_OddOccurrencesInArray.js

--- Basic/Codility_OddOccurrencesInArray.js
+++ Basic/Codility_OddOccurrencesInArray.js
@@ -18,3 +18,9 @@ function solution(A) {
 
 // 깨달은 점: Object.entries(hash)로 변환해서 find, map하는 것보다 for in으로 찾는게 빠르다.
 // (당연히 변환 과정이 없으니까 빠름. 괜히 entries 변환 하지말고 for문 돌리자...)
+
+// hash 더 간단히 만들기: reduce를 사용한다.
+const totalCounter = A.reduce((counter, num) => {
+  counter[num] = counter[num] ? counter[num] + 1 : 1;
+  return counter;
+}, {});
