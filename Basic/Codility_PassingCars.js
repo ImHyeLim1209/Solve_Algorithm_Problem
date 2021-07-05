38871af2484976fab31eef878e27583ff5d1469b
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2021-07-05 11:45:39 +0900

    Update Codility_PassingCars.js

--- Basic/Codility_PassingCars.js
+++ Basic/Codility_PassingCars.js
@@ -21,6 +21,31 @@ function solution(A) {
   return cnt;
 }
 
+//유사답안 
+// i가 0이면 i 이후에 있는 1들은 모두 i와 쌍이 된다.
+// 1. 전체 1의 갯수를 센다.
+// 2. A를 돌면서 1을 카운팅한다.
+// 3. A를 돌면서 0이 나오면 쌍을 만들고 전체 1의 갯수에서 카운팅된 1을 뺀다
+
+function solution(A) {
+    let numOfOne = A.filter((v) => v === 1).length;
+    let pairs = 0;
+    let cnt = 0;
+    for(let i = 0; i<A.length; i++) {
+        if(A[i] === 1) cnt++;
+        else if(A[i] === 0) {
+            numOfOne -= cnt;
+            pairs += numOfOne;
+            cnt = 0;
+        }
+
+        if(pairs > 1000000000) return -1
+    }
+    numOfOne -= cnt;
+    return pairs;
+}
+
+
 //다른 답안: 1을 기준으로 순서쌍 만들기(Java) -> 1을 기준으로 하므로 0의 갯수를 앞에서 부터 세면 된다.
 public int solution(int[] A) {
   int pCount = 0;
